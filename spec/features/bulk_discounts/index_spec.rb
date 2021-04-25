deb0bcbda2e55a7c1b1dc11c75e4674bf24e79a4
Aidan McGuire <>
2021-04-24 21:06:02 -0600

    Add links to bulk_discount show pages

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'bulk discounts show page' do
     expect(page).to have_content(bulk_discount1.threshold)
     expect(page).to have_content(bulk_discount2.threshold)
     expect(page).to have_content(bulk_discount3.threshold)
-    
+    save_and_open_page
     expect(page).to_not have_content(bulk_discount4.threshold)
     expect(page).to_not have_content(bulk_discount5.threshold)
     expect(page).to_not have_content(bulk_discount6.threshold)
@@ -63,5 +63,34 @@ RSpec.describe 'bulk discounts show page' do
       expect(current_path).to eq("/merchants/#{merchant1.id}/bulk_discounts")
     end
     expect(page).to_not have_content("#{bulk_discount3.discount}")
+  end
+
+  it "shows the bulk discounts id that is a link to it's show page" do
+    merchant1 = Merchant.create!(name: 'Big Merch')
+    
+    bulk_discount1 = merchant1.bulk_discounts.create!(threshold: 5, discount: 0.1)
+    bulk_discount2 = merchant1.bulk_discounts.create!(threshold: 10, discount: 0.2)
+    bulk_discount3 = merchant1.bulk_discounts.create!(threshold: 15, discount: 0.3)
+
+    visit "/merchants/#{merchant1.id}/bulk_discounts"
+    within("#bulk_discount-#{bulk_discount1.id}") do
+      expect(page).to have_link("#{bulk_discount1.id}")
+      click_link "#{bulk_discount1.id}"
+      expect(current_path).to eq("/merchants/#{merchant1.id}/bulk_discounts/#{bulk_discount1.id}")
+    end
+
+    visit "/merchants/#{merchant1.id}/bulk_discounts"
+    within("#bulk_discount-#{bulk_discount2.id}") do
+      expect(page).to have_link("#{bulk_discount2.id}")
+      click_link "#{bulk_discount2.id}"
+      expect(current_path).to eq("/merchants/#{merchant1.id}/bulk_discounts/#{bulk_discount2.id}")
+    end
+    
+    visit "/merchants/#{merchant1.id}/bulk_discounts"
+    within("#bulk_discount-#{bulk_discount3.id}") do
+      expect(page).to have_link("#{bulk_discount3.id}")
+      click_link "#{bulk_discount3.id}"
+      expect(current_path).to eq("/merchants/#{merchant1.id}/bulk_discounts/#{bulk_discount3.id}")
+    end
   end
 end
