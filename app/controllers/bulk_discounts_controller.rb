13429320624e3300586c4197a72d7f4ca0c6fd50
Aidan McGuire <>
2021-04-24 22:16:47 -0600

    Refactor bulk_discounts_controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,32 +1,44 @@
 class BulkDiscountsController < ApplicationController
-  def index
+  before_action(:set_merchant, only: [:index, :show, :create, :destroy])
+  before_action(:set_bulk_discount, only: [:show, :edit, :destroy])
+
+  def set_bulk_discount
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def set_merchant
     @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def index
     @bulk_discounts = @merchant.bulk_discounts
   end
 
   def show
-    @bulk_discount = BulkDiscount.find(params[:id])
+    @bulk_discount
+    @merchant
   end
 
   def new
   end
 
   def create
-    merchant = Merchant.find(params[:merchant_id])
-    bulk_discount = merchant.bulk_discounts.new(bulk_discount_params)
+    bulk_discount = @merchant.bulk_discounts.new(bulk_discount_params)
     if bulk_discount.save
-      redirect_to "/merchants/#{merchant.id}/bulk_discounts", notice: "Bulk Discount Successfully Created"
+      redirect_to "/merchants/#{@merchant.id}/bulk_discounts", notice: "Bulk Discount Successfully Created"
     else
-      redirect_to "/merchants/#{merchant.id}/bulk_discounts/new"
+      redirect_to "/merchants/#{@merchant.id}/bulk_discounts/new"
       flash[:alert] = "Error: #{error_message(bulk_discount.errors)}"
     end
   end
 
+  def edit
+    @bulk_discount
+  end
+
   def destroy
-    merchant = Merchant.find(params[:merchant_id])
-    bulk_discount = BulkDiscount.find(params[:id])
-    merchant.bulk_discounts.delete(bulk_discount)
-    redirect_to "/merchants/#{merchant.id}/bulk_discounts"
+    @merchant.bulk_discounts.delete(@bulk_discount)
+    redirect_to "/merchants/#{@merchant.id}/bulk_discounts"
   end
 
   private
